f7c9e17b8657233352a732759336b9873db673f7
Sébastien Laporte <sebastien.laporte@harmonie-mutuelle.fr>
2021-11-30 09:56:10 +0100

    :bug: Use base and head reference from pull request to create merge

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,35 +6,31 @@ echo "-----------------------------------------------------------"
 echo "Processing $GITHUB_EVENT_PATH"
 echo "-----------------------------------------------------------"
 
-echo "### Printing environment"
-env
-
 echo "### Printing $GITHUB_EVENT_PATH"
 jq . "$GITHUB_EVENT_PATH"
 
-echo "### Making sure this is a pull prequest by looking for the PR URL"
+echo "### Making sure this is a pull request by looking for the PR URL"
 (jq -r ".pull_request.issue.href" "$GITHUB_EVENT_PATH") || exit 78
 
-echo "### Getting the head branch"
-HEAD_BRANCH=$(jq -r .pull_request.base.repo.default_branch "$GITHUB_EVENT_PATH")
+echo "### Getting head branch"
+HEAD_BRANCH=$(jq -r .pull_request.head.ref "$GITHUB_EVENT_PATH")
 
-echo "### Getting branch to merge"
-BRANCH_TO_MERGE=$(jq -r .pull_request.head.ref "$GITHUB_EVENT_PATH")
+echo "### Getting base branch"
+BASE_BRANCH=$(jq -r .pull_request.base.ref "$GITHUB_EVENT_PATH")
 
-echo "### HEAD_BRANCH     = ${HEAD_BRANCH}"
-echo "### BRANCH_TO_MERGE = ${BRANCH_TO_MERGE}"
+echo "### HEAD_BRANCH = ${HEAD_BRANCH}"
+echo "### BASE_BRANCH = ${BASE_BRANCH}"
 
-echo "### Merging the Pull Request"
+echo "### Merging Pull Request"
 git remote set-url origin https://x-access-token:${GITHUB_TOKEN}@${GITHUB_HOST:-github.com}/${GITHUB_REPOSITORY}.git
 git config --global user.email "actions@github.com"
 git config --global user.name "GitHub Merge Action"
 
 set -o xtrace
 
-git fetch origin $HEAD_BRANCH
+git fetch origin $BASE_BRANCH
 
 # do the merge
-git checkout -b $HEAD_BRANCH origin/$HEAD_BRANCH
-git merge origin/$BRANCH_TO_MERGE --no-edit
-git push origin $HEAD_BRANCH
-git push origin --delete $BRANCH_TO_MERGE
+git checkout -b $BASE_BRANCH origin/$BASE_BRANCH
+git merge origin/$HEAD_BRANCH --no-edit
+git push origin $BASE_BRANCH
